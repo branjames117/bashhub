907b7f98df2a2d6c34ef3a32b91d9ba54fc74776
Brandon Hoskins <branjames117@gmail.com>
2022-03-22 22:51:16 -0500

    deploy to heroku

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -3,10 +3,14 @@ import io from 'socket.io-client';
 import Auth from '../utils/auth';
 // import { SOCKET_URL } from 'config';
 
-// export const socket = io.connect();
-export const socket = io.connect('http://localhost:3001', {
+export const socket = io.connect({
   query: {
     token: Auth.getToken(),
   },
 });
+// export const socket = io.connect('http://localhost:3001', {
+//   query: {
+//     token: Auth.getToken(),
+//   },
+// });
 export const SocketContext = createContext();
